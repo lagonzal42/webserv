b0e74ee2f0630a42724a43c7d671017aaf11e57d
nimai <119419194+inozo22@users.noreply.github.com>
2024-06-01 17:24:42 +0200

    added responseGenerator_DELETE error cases

--- webserv42/inc/ResponseGenerator_DELETE.hpp
+++ webserv42/inc/ResponseGenerator_DELETE.hpp
@@ -21,6 +21,8 @@ class ResponseGeneratorDELETE
 	// --- Getter functions --- //
 			// I don't know why does work well with std::string, and not with char *
 			static std::string generateDeleteResponse(Request & req, const Parser::Location & currentLoc, const std::string &fullPath);
+			static std::string generateHttpResponse(const std::string &status, const std::string &title, const std::string &message);
+
 			// static const char	* generateDeleteResponse(Request & req, const Parser::Location & currentLoc, const std::string &fullPath);
 	// --- Getter functions --- //
 
